d0e11aac60dfbb8dd50dc31bd9e0dbfb362ba79b
Robert Johansson <robert.johansson.86@hotmail.com>
2026-09-10 16:15:58 +0200

    feat(CategoriesController): add category creation action

--- BlixthackByMordor/Controllers/CategoriesController.cs
+++ BlixthackByMordor/Controllers/CategoriesController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BlixthackByMordor.Models;
+using BlixthackByMordor.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlixthackByMordor.Controllers
 {
-    public class CategoriesController : Controller
+    public class CategoriesController(CategoryService categoryService) : Controller
     {
         private bool IsAdmin() => User.IsInRole("Admin");
         public IActionResult Index()
@@ -18,5 +20,21 @@ namespace BlixthackByMordor.Controllers
 
             return View();
         }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(string categoryName)
+        {
+            var category = new CategoryModel()
+            {
+                Name = categoryName,
+                CreatedAt = DateTime.Now
+
+            };
+            await categoryService.CreateNewCategory(category);
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
