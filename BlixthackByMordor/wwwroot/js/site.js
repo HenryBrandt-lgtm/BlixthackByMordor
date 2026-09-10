060fe902bee5159725be5c420718bc4166d63760
Myyar Hasan <myyar2012@gmail.com>
2026-09-10 21:33:16 +0200

    feat(favorite): add JS function to the checkbox

--- BlixthackByMordor/wwwroot/js/site.js
+++ BlixthackByMordor/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.querySelectorAll(".favorite-checkbox").forEach(checkbox => {
+
+    checkbox.addEventListener("change", async function () {
+
+        const answerId = this.dataset.answerId;
+        console.log("Answer ID:", answerId);
+
+        const response = await fetch(`/UserFavorite/ToggleFavorite?answerId=${answerId}`, {
+            method: "POST"
+
+        });
+        console.log("Status:", response.status);
+
+        if (!response.ok) {
+            this.checked = !this.checked;
+            return;
+        }
+
+        const result = await response.json();
+        console.log("Server response:", result);
+
+        this.checked = result.isFavorite;
+        if (!result.isFavorite) {
+            this.closest(".thread-list__item").remove();
+        }
+
+    });
+
+});
