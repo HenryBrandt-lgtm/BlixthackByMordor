177141901d41da8f698bf3f54e069d18ab8ea702
Robert Johansson <robert.johansson.86@hotmail.com>
2026-09-10 15:45:20 +0200

    feat(Categories): add admin access to create category

--- BlixthackByMordor/Controllers/CategoriesController.cs
+++ BlixthackByMordor/Controllers/CategoriesController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlixthackByMordor.Controllers
 {
     public class CategoriesController : Controller
     {
+        private bool IsAdmin() => User.IsInRole("Admin");
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult CreateCategory()
+        {
+            if (!IsAdmin()) return Forbid();
+
             return View();
         }
     }
